f3c05af357421a528e8906ea5021b46d94c0c496
signekatt <liljaanton2001@gmail.com>
2019-09-16 21:17:20 +0200

    Added error check to pixmap_t ctor

--- src/drawable.cpp
+++ src/drawable.cpp
@@ -10,8 +10,16 @@ namespace xcbw {
                        uint16_t height)
         : m_id(xcb_generate_id(get(c))) {
 
-        xcb_create_pixmap(get(c), depth, static_cast<xcb_pixmap_t>(m_id),
-                          static_cast<xcb_drawable_t>(drawable), width, height);
+        const auto cookie =
+            xcb_create_pixmap_checked(get(c), depth, static_cast<xcb_pixmap_t>(m_id),
+                                      static_cast<xcb_drawable_t>(drawable), width, height);
+
+        if (auto* err = xcb_request_check(get(c), cookie)) {
+            const auto code = err->error_code;
+            free(err);
+            throw std::runtime_error(
+                (std::string("XCB: Cannot create pixmap_t : ") + std::to_string(code)));
+        }
     }
 
     void pixmap_t::free_pixmap(connection_t& c) {
